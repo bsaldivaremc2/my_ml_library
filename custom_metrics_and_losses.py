9db8f248f1c416e5120bcbdf3475d54e3fe40b19
Bryan Saldivar <bsaldivar.emc2@gmail.com>
2024-02-02 09:53:53 +0100

    Update custom_metrics_and_losses.py

--- custom_metrics_and_losses.py
+++ custom_metrics_and_losses.py
@@ -1,6 +1,6 @@
 import torch
 
-def differentiable_tanimoto_distance(u,v,sharpness=0.0005,e=1e-8,dim=-1):
+def differentiable_tanimoto_distance(u,v,input_sharpenss=1000000,sharpness=0.0005,e=1e-8,dim=-1):
   """"
   Computes a differentiable approximation of the tanimoto distance that could be used with pytorch and gpus: 
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.jaccard.html#scipy.spatial.distance.jaccard
@@ -9,9 +9,11 @@ def differentiable_tanimoto_distance(u,v,sharpness=0.0005,e=1e-8,dim=-1):
   e: to avoid division by zero.
   It sums on the last dimension so keeps the batch dimension for parallel processing.
   """
+  t1 = torch.sigmoid((u-0.5)*input_sharpenss)
+  t2 = torch.sigmoid((v-0.5)*input_sharpenss)
   te = torch.tensor(e,dtype=torch.float32, requires_grad=False)
-  nonzero = torch.pow(torch.sigmoid(u+v)-0.5,sharpness)
-  different = torch.pow(u-v ,2)
+  nonzero = torch.pow(torch.sigmoid(t1+t2)-0.5,sharpness)
+  different = torch.pow(t1-t2 ,2)
   unequal_nonzero = different * nonzero
   a = torch.sum(unequal_nonzero,dim)
   b = torch.sum(nonzero,dim)
