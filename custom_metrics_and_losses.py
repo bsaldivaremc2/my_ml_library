228f40884dc7a3173a1d82bac91de94367ff43c1
Bryan Saldivar <bsaldivar.emc2@gmail.com>
2024-02-05 11:58:19 +0100

    Update custom_metrics_and_losses.py

--- custom_metrics_and_losses.py
+++ custom_metrics_and_losses.py
@@ -1,20 +1,15 @@
 import torch
 
-def differentiable_tanimoto_distance(u,v,input_sharpenss=1000000,sharpness=0.0005,e=1e-8,dim=-1):
+def differentiable_tanimoto_distance(target,pred):
   """"
-  Computes a differentiable approximation of the tanimoto distance that could be used with pytorch and gpus: 
+  Computes a differentiable approximation of the tanimoto distance / Intersection Over Union, that could be used with pytorch and gpus: 
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.jaccard.html#scipy.spatial.distance.jaccard
     https://github.com/scipy/scipy/blob/v1.12.0/scipy/spatial/distance.py#L756-L827
-  the number of decimals in sharpeness will aproximate it closer to 1. 0.0005 makes 0.999, 0.005 makes 0.99 (Not complete 1s)
-  e: to avoid division by zero.
   It sums on the last dimension so keeps the batch dimension for parallel processing.
   """
-  t1 = torch.sigmoid((u-0.5)*input_sharpenss)
-  t2 = torch.sigmoid((v-0.5)*input_sharpenss)
-  te = torch.tensor(e,dtype=torch.float32, requires_grad=False)
-  nonzero = torch.pow(torch.sigmoid(t1+t2)-0.5,sharpness)
-  different = torch.pow(t1-t2 ,2)
-  unequal_nonzero = different * nonzero
-  a = torch.sum(unequal_nonzero,dim)
-  b = torch.sum(nonzero,dim)
-  return a/(b+te)
+  pred = torch.relu(pred-0.5)*2
+  math_and = pred * target
+  intersection = torch.sum(math_and,dim=-1)
+  union = torch.sum(pred + target - math_and,dim=-1)
+  iou = intersection / union
+  return iou
